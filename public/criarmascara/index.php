01410bf4293860fe793820a1c4b8b75221ca1eb5
Fernando Mendes <fernando.mendes@webca.com.br>
2013-10-31 22:51:03 -0200

    tipos

--- public/criarmascara/index.php
+++ public/criarmascara/index.php
@@ -14,18 +14,14 @@ padding-bottom: 40px;
 </style>
 
 <body style="margin: 20px; padding: 20px;">
-
+			Iphone4/4S
 			<div style="margin: auto; width: 400px;">
-
 				<div style="width:240px;">
                     <div class="image">
                         <div style="background: url(<?php echo $imagem; ?>) top center; height: 468px; background-size: 468px; width: 240px;border-radius: 46px;"></div>
                     </div>
-
                     <img src="mascara_iphone_4s.png" style="height: 468px;width: 240px;position: relative;top: -508px;margin: 0 0 -254px;   border-radius: 44px!important;" alt="">
-
                 </div>
-
             </div>
 
 </body>
