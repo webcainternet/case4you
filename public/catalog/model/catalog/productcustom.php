ec31f07f66f5e344b003dc572e25c883b466fa57
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-01 09:14:38 -0200

    f

--- public/catalog/model/catalog/productcustom.php
+++ public/catalog/model/catalog/productcustom.php
@@ -1,5 +1,5 @@
 <?php
-class ModelCatalogProduct extends Model {
+class ModelCatalogProductcustom extends Model {
 	public function updateViewed($product_id) {
 		$this->db->query("UPDATE " . DB_PREFIX . "product SET viewed = (viewed + 1) WHERE product_id = '" . (int)$product_id . "'");
 	}
