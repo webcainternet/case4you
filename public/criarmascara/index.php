510bc9a1baa12b03c28d572edd8825523cd0f467
Fernando Mendes <fernando.mendes@webca.com.br>
2013-10-31 22:26:38 -0200

    mascara

---
+++ public/criarmascara/index.php
@@ -0,0 +1,35 @@
+<?php
+	$imagem = $_GET["imagem"];
+?>
+
+<html>
+
+<style>
+
+.image {
+/* background: url(shadow-kpa.png) no-repeat bottom center; 
+background-size: 250px; */
+padding-bottom: 40px;
+}
+</style>
+
+<body style="margin: 20px; padding: 20px;">
+
+			<div style="margin: auto; width: 400px;">
+
+				<div style="width:240px;">
+                    <div class="image">
+                        <div style="background: url(<?php echo $imagem; ?>) top center;height: 468px; width: 240px;border-radius: 46px;"></div>
+                    </div>
+
+                    <img src="mascara_iphone_4s.png" style="height: 468px;width: 240px;position: relative;top: -508px;margin: 0 0 -254px;   border-radius: 44px!important;" alt="">
+
+                </div>
+
+            </div>
+
+</body>
+
+
+
+</html>
