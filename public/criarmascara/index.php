6d0e8db983ce335772430814880de165628f78be
Fernando Mendes <fernando.mendes@webca.com.br>
2013-10-31 23:00:08 -0200

    center

--- public/criarmascara/index.php
+++ public/criarmascara/index.php
@@ -19,7 +19,7 @@ padding-bottom: 40px;
 				Iphone4/4S<br />&nbsp;
 				<div style="width:240px;">
                     <div class="image">
-                        <div style="background: url(<?php echo $imagem; ?>); background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
+                        <div style="background: url(<?php echo $imagem; ?>) center; background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
                     </div>
                     <img src="mascara_iphone_4s.png" style="height: 468px;width: 240px;position: relative;top: -508px;margin: 0 0 -254px;   border-radius: 44px!important;" alt="">
                 </div>
@@ -30,7 +30,7 @@ padding-bottom: 40px;
 				Iphone5/5S<br />&nbsp;
 				<div style="width:240px;">
                     <div class="image">
-                        <div style="background: url(<?php echo $imagem; ?>); background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
+                        <div style="background: url(<?php echo $imagem; ?>) center; background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
                     </div>
                     <img src="mascara_iphone_4s.png" style="height: 468px;width: 240px;position: relative;top: -508px;margin: 0 0 -254px;   border-radius: 44px!important;" alt="">
                 </div>
@@ -41,7 +41,7 @@ padding-bottom: 40px;
 				GalaxyS3<br />&nbsp;
 				<div style="width:240px;">
                     <div class="image">
-                        <div style="background: url(<?php echo $imagem; ?>); background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
+                        <div style="background: url(<?php echo $imagem; ?>) center; background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
                     </div>
                     <img src="mascara_iphone_4s.png" style="height: 468px;width: 240px;position: relative;top: -508px;margin: 0 0 -254px;   border-radius: 44px!important;" alt="">
                 </div>
@@ -52,7 +52,7 @@ padding-bottom: 40px;
 				GalaxyS4<br />&nbsp;
 				<div style="width:240px;">
                     <div class="image">
-                        <div style="background: url(<?php echo $imagem; ?>); background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
+                        <div style="background: url(<?php echo $imagem; ?>) center; background-size: auto 100%; height: 468px; width: 240px;border-radius: 46px;"></div>
                     </div>
                     <img src="mascara_iphone_4s.png" style="height: 468px;width: 240px;position: relative;top: -508px;margin: 0 0 -254px;   border-radius: 44px!important;" alt="">
                 </div>
