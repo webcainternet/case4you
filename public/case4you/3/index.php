f26a305eeb47cc33ff17e54c9bd8929acee23799
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-01 16:10:10 -0200

    a

--- public/case4you/3/index.php
+++ public/case4you/3/index.php
@@ -72,10 +72,10 @@ background-color: transparent;
 
 	function selecionarfiltro(meutemplate, minhaext) {
 
-        if (minhaext == "") { document.getElementById('filtrocapi').value = '0'; }
-        if (minhaext == "-pb.png") { document.getElementById('filtrocapi').value = '1'; }
-        if (minhaext == "-sp.png") { document.getElementById('filtrocapi').value = '2'; }
-        if (minhaext == "-40.png") { document.getElementById('filtrocapi').value = '3'; }
+        if (minhaext == "") { document.getElementById('filtrocapi').value = "0"; }
+        if (minhaext == "-pb.png") { document.getElementById('filtrocapi').value = "1"; }
+        if (minhaext == "-sp.png") { document.getElementById('filtrocapi').value = "2"; }
+        if (minhaext == "-40.png") { document.getElementById('filtrocapi').value = "3"; }
 
                 if (meutemplate == '0') {
                         varbg = document.getElementById("divl1").style.backgroundImage;
