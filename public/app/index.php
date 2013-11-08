9cd48aa619e9195ae9b861ef47baee6451cddefc
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-08 18:30:39 -0200

    aaa

--- public/app/index.php
+++ public/app/index.php
@@ -18,7 +18,7 @@
 
 	<div class="wrap-app">
 		
-		<div id="accordion" style="text-align: left; height: 700px;">
+		<div id="accordion" style="text-align: left; height: 700px; width: 400px;">
 			<!-- passo 1 -->
 			<h3>&nbsp;
     			<div style="float: left; width: 50%;">Passo 1 - Modelo do seu celular</div>
@@ -26,28 +26,142 @@
   			</h3>
 
   			<div>
-  				Passo1
+
+  				<div class="c4ystepnumber">1</div>
+  				<div class="fontc4y2 c4ysteptitle">SELECIONE O MODELO DE SEU CELULAR!</div>
+				<div class="fontc4y1 c4ysteptext">AJUDA: Temos diversas opções como Iphone4/4S, Iphone5/5S, Galaxy S3, Galaxy S4, e em breve outros modelos!</div>
+
+				<div style="float: left; text-align: left; width: 95px; margin-bottom: 20px; margin-left: 15px;">
+						<div style="float: left; text-align: left">
+							<input onChange="selecionarmodelo(this)" type="radio" name="modelo" id="miphone4" value="iphone4">
+						</div>
+					          <div style="float: left; text-align: left;padding-left: 5px; height: 90px;">
+							<img style="width: 39px;" src="./img/iphone4.png" alt="">
+					          </div>
+					          <div class="fontc4y1" style="float: left; text-align: left; padding-left: 20px;margin-top: 5px;">
+							Iphone4/4S
+						</div>
+				</div>
+
+				<div style="float: left; text-align: left; width: 95px; margin-bottom: 20px; margin-left: 15px;">
+				        <div style="float: left; text-align: left">
+				                <input onChange="selecionarmodelo(this)" type="radio" name="modelo" id="miphone5" value="iphone5">
+				        </div>
+				        <div style="float: left; text-align: left;padding-left: 5px; height: 90px;">
+				                <img style="width: 49px;" src="./img/iphone5.png" alt="">
+				        </div>
+				        <div class="fontc4y1" style="float: left; text-align: left; padding-left: 20px;margin-top: 5px;">
+				                Iphone5/5S
+				        </div>
+				</div>
+
+				<div style="float: left; text-align: left; width: 95px; margin-bottom: 20px; margin-left: 15px;">
+				        <div style="float: left; text-align: left">
+				                <input onChange="selecionarmodelo(this)" type="radio" name="modelo" id="mgalaxy3" value="galaxy3">
+				        </div>
+				        <div style="float: left; text-align: left;padding-left: 5px; height: 90px;">
+				                <img style="width: 45px;" src="./img/galaxys3.jpg" alt="">
+				        </div>
+				        <div class="fontc4y1" style="float: left; text-align: left; padding-left: 20px;margin-top: 5px;">
+				                Galaxy S3
+				        </div>
+				</div>
+
+				<div style="float: left; text-align: left; width: 95px; margin-bottom: 20px; margin-left: 15px;">
+				        <div style="float: left; text-align: left">
+				                <input onChange="selecionarmodelo(this)" type="radio" name="modelo" id="mgalaxy4" value="galaxy4">
+				        </div>
+				        <div style="float: left; text-align: left;padding-left: 5px; height: 90px;">
+				                <img style="width: 45px;" src="./img/galaxys4.jpg" alt="">
+				        </div>
+				        <div class="fontc4y1" style="float: left; text-align: left; padding-left: 20px;margin-top: 5px;">
+				                Galaxy S4
+				        </div>
+				</div>
 			</div>
 
-			<!-- passo 1 -->
+
+
+
+
+
+
+
+
+
+
+			<!-- passo 2 -->
 			<h3>&nbsp;
-    			<div style="float: left; width: 50%;">Passo 1 - Modelo do seu celular</div>
-    			<div style="float: right; width: 40%; text-align: right; margin-right: 20px; font-weight: 100; color: #6aa11a;"><u>Voltar - Passo 1</u></div>
+    			<div style="float: left; width: 50%;">Passo 2 - Escolha de layout</div>
+    			<div style="float: right; width: 40%; text-align: right; margin-right: 20px; font-weight: 100; color: #6aa11a;"><u>Voltar - Passo 2</u></div>
+  			</h3>
+
+  			<div>
+
+  				<div class="c4ystepnumber">2</div>
+  				<div class="fontc4y2 c4ysteptitle">ESCOLHA O TIPO DE LAYOUT!</div>
+				<div class="fontc4y1 c4ysteptext">AJUDA: Temos diversas opções como Iphone4/4S, Iphone5/5S, Galaxy S3, Galaxy S4, e em breve outros modelos!</div>
+
+			</div>
+
+
+
+
+
+			<!-- passo 3 -->
+			<h3>&nbsp;
+    			<div style="float: left; width: 50%;">Passo 3 - Selecionar Fotos</div>
+    			<div style="float: right; width: 40%; text-align: right; margin-right: 20px; font-weight: 100; color: #6aa11a;"><u>Voltar - Passo 3</u></div>
+  			</h3>
+
+  			<div>
+
+  				<div class="c4ystepnumber">2</div>
+  				<div class="fontc4y2 c4ysteptitle">ESCOLHA O TIPO DE LAYOUT!</div>
+				<div class="fontc4y1 c4ysteptext">AJUDA: Temos diversas opções como Iphone4/4S, Iphone5/5S, Galaxy S3, Galaxy S4, e em breve outros modelos!</div>
+
+			</div>
+
+
+
+
+
+			<!-- passo 4 -->
+			<h3>&nbsp;
+    			<div style="float: left; width: 50%;">Passo 4 - Selecionar Fotos</div>
+    			<div style="float: right; width: 40%; text-align: right; margin-right: 20px; font-weight: 100; color: #6aa11a;"><u>Voltar - Passo 4</u></div>
+  			</h3>
+
+  			<div>
+
+  				<div class="c4ystepnumber">2</div>
+  				<div class="fontc4y2 c4ysteptitle">ESCOLHA O TIPO DE LAYOUT!</div>
+				<div class="fontc4y1 c4ysteptext">AJUDA: Temos diversas opções como Iphone4/4S, Iphone5/5S, Galaxy S3, Galaxy S4, e em breve outros modelos!</div>
+
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+			<!-- passo 4 -->
+			<h3>&nbsp;
+    			<div style="float: left; width: 50%;">Passo 4 - Aplicar filtros</div>
+    			<div style="float: right; width: 40%; text-align: right; margin-right: 20px; font-weight: 100; color: #6aa11a;"><u>Voltar - Passo 4</u></div>
   			</h3>
 
   			<div>
   				Passo1
 			</div>
 
-			<!-- passo 1 -->
-			<h3>&nbsp;
-    			<div style="float: left; width: 50%;">Passo 1 - Modelo do seu celular</div>
-    			<div style="float: right; width: 40%; text-align: right; margin-right: 20px; font-weight: 100; color: #6aa11a;"><u>Voltar - Passo 1</u></div>
-  			</h3>
-
-  			<div>
-  				Passo1
-			</div>
 		</div>
 	</div>
 
